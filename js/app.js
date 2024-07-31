12426c1de40ed11065ec01a121eefc65e0a5c3d4
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-07-30 19:20:10 -0700

    step 1 pass

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+// Our global state object
+let state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+}
+
+let timer, gameOver; // This will assign the timer and gameOver a value of undefined
 
 
 
